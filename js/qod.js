e6efa1fcbff91d469d5b797d399b580dbe2e9e87
Abdullah Rahman <abdullahrahh@gmail.com>
2019-11-19 14:02:39 -0800

    init

--- js/qod.js
+++ js/qod.js
@@ -1,24 +1,27 @@
-(function($){
+(function($) {
+  $('body').append('hello world');
+  // your code goes here
 
-    $('body').append('hello world');
-    // your code goes here
+  // 1: get request to grab random post and append to the DOM
 
-    // 1: get request to grab random post and append to the DOM
-
-    // add a click event for the "Show Me Another" btn and then run the AJAX code below
-    $.ajax({
-        method: "GET",
-        url: // qod_vars.rest_url + /wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1
-    }).done(function(data){
-        console.log(data);
-        // append the quote to the DOM
-    }).fail(function(error){
-        console.log("an error occurred", error);
+  // add a click event for the "Show Me Another" btn and then run the AJAX code below
+  $.ajax({
+    method: 'GET',
+    url: '' // qod_vars.rest_url + /wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1
+  })
+    .done(function(data) {
+      console.log(data);
+      // append the quote to the DOM
+    })
+    .fail(function(error) {
+      console.log('an error occurred', error);
+    })
+    .on('click', 'button', function() {
+      alert('WORKING!');
     });
 
-    // 2: post a new quote using the post method
-    // using a form to submit a quote so a .submit event
-
+  // 2: post a new quote using the post method
+  // using a form to submit a quote so a .submit event
 })(jQuery);
 
 // IIFE Immediatley Invoked Function Expression
